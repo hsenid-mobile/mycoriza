bd8d6a35b3d245a75f3fb6d8ae2cfbfeb90794cc
tiranw <tiranw@hsenidmobile.com>
2021-08-30 11:58:21 +0530

    feat(runtime): Add debounce logic.

--- runtime/src/index.tsx
+++ runtime/src/index.tsx
@@ -535,3 +535,26 @@ export function useEager<T, F extends (...args: any) => void>(data: MycorizaHook
   }, [])
   return data;
 }
+
+export function useDebounce<T, F extends (...args: any) => void>(data: MycorizaHookResultType<T, F>, delay: number): MycorizaHookResultType<T, F> {
+
+  let [params, setParams] = useState<Parameters<typeof data[1]>>();
+  let [debouncedParams, setDebouncedParams] = useState<Parameters<typeof data[1]>>();
+
+  useEffect(() => {
+    let timeout = setTimeout(() => setDebouncedParams(params), delay);
+    return () => clearTimeout(timeout)
+  }, [params])
+
+  useEffect(() => {
+    if (debouncedParams) {
+      data[1](debouncedParams)
+    }
+  }, [debouncedParams])
+
+  return [
+    data[0],
+    ((...args: Parameters<typeof data[1]>) => setParams(args)) as F,
+    data[2]
+  ]
+}
