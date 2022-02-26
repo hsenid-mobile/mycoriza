4e378112a5522b217072add5e22c0126e29fc4d4
tiranw <tiranw@hsenidmobile.com>
2022-02-26 08:32:51 +0530

    fix(cli): Fix missing parameter type import.

--- packages/mycoriza/src/api/index.ts
+++ packages/mycoriza/src/api/index.ts
@@ -1,15 +1,11 @@
 
-import type { PetstoreState } from './petstore/reducers'
-import { petstoreReducers } from './petstore/reducers'
 import {combineReducers, ReducersMapObject} from "redux";
 
 export type MycorizaState<T> = {
-    petstore: PetstoreState
 } & T
 
 export function mycorizaMapObject<T>(reducers: ReducersMapObject<T>): ReducersMapObject<MycorizaState<T>> {
     return {
-            petstore: petstoreReducers,
         ...reducers
     } as any
 }
